4a1cce5e4345ec8ef453dffd315c9aedec13ec7c
Martin Spusta <lucas.spusta@gmail.com>
2020-07-10 11:30:21 -0400

    Initial Commit

---
+++ SwiftUIBiometrics/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  SwiftUIBiometrics
+//
+//  Created by Lucas Spusta on 7/10/20.
+//
+
+import SwiftUI
+import LocalAuthentication
+struct ContentView: View {
+    @State private var isUnlocked = false
+    var body: some View {
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
